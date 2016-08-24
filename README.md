0b6f58d0d80d54547739c31e0bf1a363ade5c4ae
Mehdi Abaakouk <sileht@sileht.net>
2016-08-24 22:25:38 +0200

    Nexus5 mirrorlink (from LG G4) build tools

---
+++ README.md
@@ -0,0 +1,13 @@
+
+Convert MirrorLink System Apps from LG G4 stock ROM to Nexus 5
+(Should work on any 32bit ARM eabi5)
+
+Tools needs:
+
+zip
+openssl
+keytool (from openjdk-8-jre-headless for example)
+jarsigner (from openjdk-8-jdk-headless for example)
+zipalign
+baksmali (2.1.3 is provided in the repo)
+smali (2.1.3 is provided in the repo)
---
+++ repack-and-sign-apk.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Sample usage is as follows;
+# ./signapk myapp.apk debug.keystore android androiddebugkey
+# 
+# param1, APK file: Calculator_debug.apk
+# param2, keystore location: ~/.android/debug.keystore
+# param3, key storepass: android
+# param4, key alias: androiddebugkey
+
+USER_HOME=$(eval echo ~${SUDO_USER})
+
+# use my debug key default
+APK=$1
+DEX=$2
+KEYSTORE="keystore"
+STOREPASS="android"
+ALIAS="platform"
+
+
+# get the filename
+APK_BASENAME=$(basename $APK)
+SIGNED_APK="signed_"$APK_BASENAME
+
+#debug
+echo param1 $APK
+echo param2 $KEYSTORE
+echo param3 $STOREPASS
+echo param4 $ALIAS
+
+# delete META-INF folder
+zip -d $APK META-INF/\*
+cp -f $DEX classes.dex
+zip $APK classes.dex
+rm -f classes.dex
+
+# sign APK
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $KEYSTORE -storepass $STOREPASS $APK $ALIAS
+#verify
+jarsigner -verify $APK
+
+#zipalign
+zipalign -v 4 $APK $SIGNED_APK 
